87772da7da2d53351e35c6dda843d75881253b40
BenAhmad27 <dialloaboubacar834@gmail.com>
2026-01-21 23:48:38 -0500

    form done

--- progression.md
+++ progression.md
@@ -3,3 +3,4 @@
 [] Interpolating values into a template
 [] Le service
 [] les routes
+[] formulaire
--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocationInfo} from '../housinglocation';
+
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
-export class Details {
 
+export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocationInfo | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    console.log('Bouton cliqué !', this.applyForm.value);
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
 }
